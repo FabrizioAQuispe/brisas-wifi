2c8fa154f9fd35ccbccc7d16f304abc76bc1c89a
FabrizioAQuispe <alekzander.fabrizio@gmail.com>
2024-12-22 18:37:51 -0500

    WIFI_BRISAS_CORS_ENABLED

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['*'], // Dominios permitidos
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, application/json', // Encabezados permitidos
+    credentials: true, // Permitir cookies o autenticación basada en credenciales  
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
